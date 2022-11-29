65ec6aa740269b3dc3ad31042c73059799f6fcc8
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2022-11-29 07:59:07 +0000

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
 str = str[39:66] +" "+str[-22:-18]+" "+str[0:6]
