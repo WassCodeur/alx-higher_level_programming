e284cf656f9f64fa8ace2e524968f5f7ed7c543e
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2022-12-04 20:31:47 +0000

    Update 101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-# exec('import subprocess; subpocess.run(["echo #pythoniscool", shell=True])')
-exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
+exec('import subprocess; subpocess.run(["echo #pythoniscool", shell=True])')
+# exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
