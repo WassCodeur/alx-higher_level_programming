3c5bbb3eedc4e58dbaee06dab8d06cbe9d05ee0a
WassCodeur <wachioubouraima56@gmail.com>
2022-12-04 17:10:40 +0000

    update

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def main(args):
+    j = 0
+    for i in range(1, len(args)):
+        j = j + int(args[i])
+    
+    print("{}".format(j))
+
+if __name__ == "__main__":
+    main(sys.argv)
