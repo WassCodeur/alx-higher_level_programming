7f62a855ee6b4585cd972580b17573983839d2b6
WassCodeur <wachioubouraima56@gmail.com>
2022-12-04 20:28:44 +0000

    essier print

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 100-my_calculator.py """
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    if op == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif op == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif op == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif op == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    exit(0)
