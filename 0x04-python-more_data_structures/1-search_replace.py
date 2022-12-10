13178723725ddf4db26e428af43e680ab4bf9708
WassCodeur <wachioubouraima56@gmail.com>
2022-12-10 07:28:22 +0000

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
         if i == 2:
-            i = 89
+            i = replace
         new_list.append(i)
     return new_list
