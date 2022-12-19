2689657090c71315e5041014d0860a5f0179bc9d
WassCodeur <wachioubouraima56@gmail.com>
2022-12-19 11:53:48 +0000

    creating files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if isinstance(roman_string, str) is False:
+        return 0
+    else:
+        Number = 0
+        RomanNumber = {"I": 1,"V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        for i in range(len(roman_string)):
+            valueTemp = RomanNumber[roman_string[i]]
+            if i == (len(roman_string) - 1):
+                Number += valueTemp
+            else:
+                if RomanNumber[roman_string[i + 1]] > valueTemp:
+                    Number =  Number - valueTemp
+                else:
+                    Number =  Number + valueTemp
+                
+
+    return Number
---
+++ 0x04-python-more_data_structures/test.py
@@ -0,0 +1,6 @@
+def subArraySum(arr, n, s):
+    A = 0
+    for i in range(n):
+
+6
+subArraySum([1, 2,3,4], len([1, 2,3,4]), 6)
