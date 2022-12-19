a127429545809ffb034a382dc3c6089a8b66ef4e
WassCodeur <wachioubouraima56@gmail.com>
2022-12-19 19:06:18 +0000

    madory task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+        except (TypeError, ValueError):
+            x = i
+            break
+    print()
+    return x
