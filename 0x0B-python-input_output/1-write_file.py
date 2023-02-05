87c81e9a4e81e31475dcdd839c4f0d4f361a0dca
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-02-05 00:01:49 +0000

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,8 +8,6 @@ def write_file(filename="", text=""):
     """write_file function
         This function write a string to a text file and return 
         the number of characters written 
-        If the file does not exist, it will be created. 
-        If the file already exists, its contents will be overwritten.
     args:
         filename: name of the file
         text: text to write
