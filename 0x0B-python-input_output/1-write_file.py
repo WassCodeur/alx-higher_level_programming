a5e81ea1d8ae4e089efb707437a8085628d58597
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-02-05 00:04:01 +0000

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""function that reads a text file
-
+"""
+function that writes a string to a text file (UTF8) and returns the number of characters written
 """
 
 
 def write_file(filename="", text=""):
-    """write_file function
-        This function write a string to a text file and return 
-        the number of characters written 
-    args:
-        filename: name of the file
-        text: text to write
-    return: number of characters written
-
+     """ module write_file
     """
     with open(filename, mode="w", encoding='utf-8') as myfile:
         myfile.write(text)
