4d8dc2769f8ad41a9ab5beb4c91ee49d0125a3d0
WassCodeur <wachioubouraima56@gmail.com>
2023-02-06 04:34:15 +0000

    indentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
+'''
     Class Rectangle
-"""
+'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
+    '''
         Defining the Rectangle class
         Inherits from:
             Base
-    """
+    '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
@@ -21,58 +21,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
+        '''
             Returning private attribute
-        """
+        '''
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
+        '''
             Setting private attribute
-        """
-        self.setter_validation("width", value)
+        '''
         self.__width = value
 
     @property
     def height(self):
-        """
+        '''
             Returning private attribute
-        """
+        '''
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
+        '''
             Setting private attribute
-        """
+        '''
         self.__height = value
 
     @property
     def x(self):
-        """
+        '''
             Returning private attribute
-        """
+        '''
         return self.__x
 
     @x.setter
     def x(self, value):
-        """
+        '''
             Setting private attribute
-        """
-        self.setter_validation("x", value)
+        '''
         self.__x = value
 
     @property
     def y(self):
-        """
+        '''
             Returning private attribute
-        """
+        '''
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
+        '''
             Setting private attribute
-        """
+        '''
         self.__y = value
