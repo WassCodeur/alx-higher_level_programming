f9d9c94a3e38f20b3534b7e22a4cb806fd542be9
WassCodeur <wachioubouraima56@gmail.com>
2023-02-06 04:31:20 +0000

    style

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""Class Rectangle
+"""
+    Class Rectangle
 """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Defining the Rectangle class
+    """
+        Defining the Rectangle class
         Inherits from:
             Base
     """
@@ -19,50 +21,58 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Returning private attribute
+        """
+            Returning private attribute
         """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setting private attribute
+        """
+            Setting private attribute
         """
         self.setter_validation("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """Returning private attribute
+        """
+            Returning private attribute
         """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setting private attribute
+        """
+            Setting private attribute
         """
         self.__height = value
 
     @property
     def x(self):
-        """Returning private attribute
+        """
+            Returning private attribute
         """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setting private attribute
+        """
+            Setting private attribute
         """
         self.setter_validation("x", value)
         self.__x = value
 
     @property
     def y(self):
-        """Returning private attribute
+        """
+            Returning private attribute
         """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setting private attribute
+        """
+            Setting private attribute
         """
         self.__y = value
