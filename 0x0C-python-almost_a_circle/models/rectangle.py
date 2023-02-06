56fdb3dd45fa05f23a943122d84c72aa3e94ce53
WassCodeur <wachioubouraima56@gmail.com>
2023-02-06 23:09:20 +0000

    indentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,7 +122,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """method  returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-         return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id,
+        return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id,
                                                 self.__x, self.__y,
                                                 self.__width, self.__height)
 
