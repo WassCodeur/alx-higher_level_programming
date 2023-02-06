c6189396c7bd8f00183a25ddbd13cecbe57608c4
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-02-06 21:20:58 +0000

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,81 +1,110 @@
 #!/usr/bin/python3
-"""contains class Rectangle which implements Base.
+"""
+    contains class Rectangle which implements Base.
 """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ Defining the Rectangle class
-        Inherits from:
-            Base
     """
-
+        class Rectangle implements Base.
+        Methods:
+            __init__()
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            Initializes the instance of the class..
+        """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
-        """Returning private attribute
+        """
+            getter function for __width
+            Returns: width
         """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setting private attribute
         """
-        self.setter_verificator("width", value)
+            setter function for width.
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = value
 
     @property
     def height(self):
-        """Returning private attribute
+        """
+            getter function for height
+            Returns: height
         """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setting private attribute
         """
-        self.setter_verificator("height", value)
+            setter function for height
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
         self.__height = value
 
     @property
     def x(self):
-        """Returning private attribute
+        """
+            getter function for x.
+            Returns: x
         """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setting private attribute
         """
-        self.setter_verificator("x", value)
+            setter function for x.
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = value
 
     @property
     def y(self):
-        """Returning private attribute
+        """
+            getter function for y
+            Returns: y
         """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setting private attribute
         """
-        self.setter_verificator("y", value)
-        self.__y = value
+            setter function for y
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
 
-    @staticmethod
-    def setter_verificator(item, value):
-        if type(item) != int:
-            raise TypeError("{} must be an integer".format(item))
-        if item == "width" or item == "height":
-            if value < 0 :
-                raise ValueError("{} must be > 0".format(item))
-        elif item == "x" or item == "y":
-            if value < 0 :
-                raise ValueError("{} must be >= 0".format(item))
+        self.__y = value
