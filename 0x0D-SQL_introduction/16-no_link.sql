f14d68132322653db65811ef8c8415884605be17
WassCodeur <wachioubouraima56@gmail.com>
2023-02-14 12:11:31 -0800

    update

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- a script that lists all records of the table second_table
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table WHERE name IS NOT NULL
+ORDER BY score DESC;
