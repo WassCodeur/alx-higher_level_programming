267897af2c053f62cde27a75f81334e93843e195
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-03-15 15:56:25 +0000

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,8 +4,8 @@
 
 module.exports = class Rectangle {
   constructor (width, height) {
-     this.width = width;
-     this.height = height;
+    this.width = width;
+    this.height = height;
 
-   }
+  }
 };
