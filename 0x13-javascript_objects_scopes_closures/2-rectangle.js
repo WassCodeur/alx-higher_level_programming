4757ced8a5ccf81e5b679565d16227a7883a42fc
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-03-15 17:28:04 +0000

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+
+// Checks for Rectangle parameters
+
 module.exports = class Rectangle {
-    width;
-    height;
-    constructor (w, h) {
-	if (w > 0 && h > 0) {
-    this.width = w;
-	    this.height = h;
-	}
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
   }
 };
