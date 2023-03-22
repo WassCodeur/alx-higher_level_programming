4ca641419fefbba1169fe7a3fe543dfd6e7d3126
WassCodeur <wachioubouraima56@gmail.com>
2023-03-22 00:30:33 +0000

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
                            passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT DISTINCT name\
+    cur.execute("SELECT DISTINCT cities.name\
                 FROM cities\
                 JOIN states ON cities.state_id = states.id\
                 WHERE states.name = \'{}\'\
                 ORDER BY cities.id ASC".format(state))
     cities = cur.fetchall()
     for citie in cities:
-        print(citie, end="")
+        print(citie, end=" ")
     print()
     cur.close()
     conn.close()
