858f903fd8cdf8f109a21a4f9e6e51032dac2627
WassCodeur <wachioubouraima56@gmail.com>
2023-03-27 01:12:10 +0000

    fix bug in the file "10-model_state_my_get.py"

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name == stateName )\
+    state = session.query(State).filter(State.name == stateName)\
         .first()
     # print the results
     if state is None:
