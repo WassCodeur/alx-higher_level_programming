7168ba4934102493eced8cde41e5e3927017e9c0
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-04-12 20:17:27 +0000

    Update 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Write a Python script that takes in a letter and 
+    sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+
+import requests
+import sys
+"""Import resquests module"""
+
