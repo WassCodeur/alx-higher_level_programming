10004b63fc99788f42bcf46c6a6f8e065fd3685f
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-04-12 19:56:14 +0000

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,12 @@ import urllib.request
 """Import urllib module"""
 
 
+
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         status = response.read()
+
         print('Body response:')
         print("\t- type: {}".format(type(status)))
         print("\t- content: {}".format(status))
