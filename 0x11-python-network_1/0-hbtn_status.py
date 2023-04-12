bc16bd8ed67b23c6bf3230f3c069a444341f4950
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-04-12 19:57:44 +0000

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         status = response.read()
 
