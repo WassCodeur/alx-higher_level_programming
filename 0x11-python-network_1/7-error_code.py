1366372203bbbfa80e98897f55000cc1c145b34c
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-04-12 20:15:40 +0000

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+    a Python script that takes in a URL,
+    sends a request to the URL and displays the body of the response.
+"""
+
+
 import requests
 import sys
 """Import resquests module"""
