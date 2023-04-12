c50b4b3d67588216bae7edefba479852fbc43053
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-04-12 20:06:05 +0000

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+    Write a Python script that takes in a URL and an email, 
+    sends a POST request to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)
+"""
+
+
 import urllib.request
 import urllib.parse
 import sys
