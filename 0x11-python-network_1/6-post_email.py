7a23cf32ba3aaac160ee7071a9fab71688e6edcf
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-04-12 20:14:24 +0000

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+    Write a Python script that takes in a URL and an email address,
+    sends a POST request to the passed URL with the email as a parameter,
+    and finally displays the body of the response.
+"""
+
+
 import requests
 import sys
 """Import resquests module"""
