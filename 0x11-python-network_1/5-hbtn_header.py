0097a3a199df609a78f092e90e80633b320f81b6
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-04-12 20:12:23 +0000

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+    Write a Python script that takes in a URL, 
+    sends a request to the URL and displays 
+    the value of the variable X-Request-Id in the response header
+ """
 import requests
 import sys
 """Import resquests module"""
@@ -7,5 +12,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-    XRequestId = req.headers['X-Request-Id']
-    print("{}".format(XRequestId))
+    X-Request-Id = req.headers['X-Request-Id']
+    print("{}".format(X-Request-Id))
