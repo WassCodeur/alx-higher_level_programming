6495b7fa7a2ff7c0dd9c0fa7670f593d0644c668
Wachiou BOURAÏMA <100234404+WassCodeur@users.noreply.github.com>
2023-04-12 20:10:13 +0000

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+    Write a Python script that takes in a URL, 
+    sends a request to the URL and displays the body of the response (decoded in utf-8).
+"""
+
+
 import urllib.request
 import urllib.error
 import sys
