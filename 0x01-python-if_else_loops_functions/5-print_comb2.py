617ba91022b3e136bf4af5c7842aea68ffa4588c
WassCodeur <wachioubouraima56@gmail.com>
2023-05-20 18:32:37 +0000

    update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if j + i != 18:
-            print("{}{}".format(i, j), end=", ")
-        else:
-            print("{}{}".format(i, j))
+
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+print(99)
