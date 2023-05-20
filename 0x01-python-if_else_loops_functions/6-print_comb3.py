b529cf36d8a75882d2da1fd52354e8cd8b64ee4e
WassCodeur <wachioubouraima56@gmail.com>
2023-05-20 18:32:59 +0000

    update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range(0, 10):
+    for j in range(1, 10):
         if j > i:
-            if i + j != 17:
-                print("{}{}".format(i, j), end=", ")
-            else:
+            if i + j == 17:
                 print("{}{}".format(i, j))
+            else:
+                print("{}{}".format(i, j), end=", ")
