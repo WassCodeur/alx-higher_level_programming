9e5e255a947cf5e40e2914baf78d9e1405fbdaab
WassCodeur <wachioubouraima56@gmail.com>
2023-06-01 17:15:10 +0000

    Star wars Movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,19 @@
+$('document').ready(function () {
+  const list_movies = $('#list_movies');
+  $.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    method: 'GET',
+    dataType: 'json',
+    success: function (response) {
+      const movies = response.results;
+      movies.forEach(movie => {
+        const title = movie.title;
+        const listItem = $('<li>').text(title);
+        list_movies.append(listItem);
+      });
+    },
+    error: function (xhr, status, error) {
+      console.error('something went wrong', error);
+    }
+  });
+});
