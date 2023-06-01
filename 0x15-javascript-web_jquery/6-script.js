ea2391c1410830695bbcd6c3a26823952d0fb3b8
WassCodeur <wachioubouraima56@gmail.com>
2023-06-01 17:13:11 +0000

    Change the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+$('document').ready(function () {
+  const header = $('header');
+  const update_header = $('#update_header');
+  update_header.on('click', function () {
+    header.text('New Header!!!');
+  });
+});
