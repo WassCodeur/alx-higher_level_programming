e0ef22792330f826271290d7fadaa1283f0a0099
WassCodeur <wachioubouraima56@gmail.com>
2023-06-05 19:18:45 +0000

    performe task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for i in a_dictionary:
-        new_dict.update({i : a_dictionary[i]*2})
-    return new_dict
+    new_dict = a_dictionary.copy()
+    for key in new_dict:
+        new_dict[key] *= 2
+    return (new_dict)
