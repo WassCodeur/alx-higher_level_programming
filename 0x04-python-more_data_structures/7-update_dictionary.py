ebe5a951f53e694b6c123267380430c6850f8e85
WassCodeur <wachioubouraima56@gmail.com>
2023-06-05 19:17:55 +0000

    performe task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,5 @@ def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
     else:
-       a_dictionary.update({key : value})
-    return a_dictionary
+        a_dictionary.update({key: value})
+    return (a_dictionary)
