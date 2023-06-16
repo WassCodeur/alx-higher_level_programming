eba7e39a45525c753120f372edf586a2906c4abf
WassCodeur <wachioubouraima56@gmail.com>
2023-06-16 03:36:26 +0000

    fix error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = list(map(lambda x: x, my_list))
     for i in range(len(new_list)):
-        if new_list[i] == 2:
+        if new_list[i] == search:
             new_list[i] = replace
     return (list(new_list))
