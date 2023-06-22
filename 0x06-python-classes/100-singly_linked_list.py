e176af61217f43ced54075f52c03882baf8da979
WassCodeur <wachioubouraima56@gmail.com>
2023-06-22 00:50:33 +0000

    single linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,6 +3,8 @@
 
 
 class Node:
+    """Node templates"""
+
     def __init__(self, data, next_node=None):
         self._data = None
         self._next_node = None
@@ -11,30 +13,38 @@ class Node:
 
     @property
     def data(self):
+        """get current node value"""
         return self._data
 
     @data.setter
     def data(self, value):
+        """set new value to the current node"""
         if not isinstance(value, int):
             raise TypeError("data doit être un entier")
         self._data = value
 
     @property
     def next_node(self):
+        """next node"""
         return self._next_node
 
     @next_node.setter
     def next_node(self, value):
+        """set value to the next_noe"""
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node doit être un objet Node")
         self._next_node = value
 
 
 class SinglyLinkedList:
+    """class singly linked list"""
+
     def __init__(self):
+        """initialization"""
         self.head = None
 
     def sorted_insert(self, value):
+        """public method to insert a new node"""
         new_node = Node(value)
 
         if self.head is None or value < self.head.data:
@@ -50,6 +60,7 @@ class SinglyLinkedList:
             current.next_node = new_node
 
     def __str__(self):
+        """ public method __str__"""
         nodes = []
         current = self.head
         while current is not None:
