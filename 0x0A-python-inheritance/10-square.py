2354ad32b7985f21f671c563c111b3c39e48e0d2
WassCodeur <wachioubouraima56@gmail.com>
2023-06-23 22:22:35 +0000

    fix style error

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,9 +3,9 @@
 """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
-
-class Square(BaseGeometry):
+class Square(Rectangle):
     """Square class
     """
 
